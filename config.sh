96a9ca556871060c033e53bae3c3e4668e81f499
BHUTUU <raj259942@gmail.com>
2021-08-22 05:43:35 +0530

    config changes

--- config.sh
+++ config.sh
@@ -43,8 +43,8 @@ case "${OS}" in
     echo -e "\e[1;34mConfiguring.......\e[0m"
     cp -r $PREFIX/lib/python3.9/site-packages/InstagramPy/__p*  ${CWD}/assets/InstagramPy/ > /dev/null 2>&1
     cp -r $PREFIX/lib/python3.9/site-packages//InstagramPy/colors/__pycache__ ${CWD}/assets/InstagramPy/colors/ > /dev/null 2>&1
-    rm -rf $PREFIX/lib/python3.9/site-packages/InstagramPy > /dev/null 2>&1
-    cp -r assets/InstagramPy $PREFIX/lib/python3.9/site-packages > /dev/null 2>&1
+    #rm -rf $PREFIX/lib/python3.9/site-packages/InstagramPy > /dev/null 2>&1
+    #cp -r assets/InstagramPy $PREFIX/lib/python3.9/site-packages > /dev/null 2>&1
     cp -r $PREFIX/lib/python3.9/site-packages/instagram_py-2.0.7-py3.9.egg-info ${CWD}/assets
     mv -v assets/instapy-config.json $HOME > /dev/null 2>&1
     rm -rf $PREFIX/etc/tor/torrc > /dev/null 2>&1
@@ -69,8 +69,8 @@ case "${OS}" in
     echo -e "\e[1;34mConfiguring.......\e[0m"
     sudo cp -r $PREFIX/lib/python3.9/site-packages/InstagramPy/__p*  ${CWD}/assets/InstagramPy > /dev/null 2>&1
     sudo cp -r $PREFIX/lib/python3.9/site-packages//InstagramPy/colors/__pycache__ ${CWD}/assets/InstagramPy/colors/ > /dev/null 2>&1
-    sudo rm -rf $PREFIX/lib/python3.9/site-packages/InstagramPy > /dev/null 2>&1
-    sudo cp -r assets/InstagramPy $PREFIX/lib/python3.9/site-packages > /dev/null 2>&1
+    #sudo rm -rf $PREFIX/lib/python3.9/site-packages/InstagramPy > /dev/null 2>&1
+    #sudo cp -r assets/InstagramPy $PREFIX/lib/python3.9/site-packages > /dev/null 2>&1
     sudo cp -r $PREFIX/lib/python3.9/site-packages/instagram_py-2.0.7-py3.9.egg-info ${CWD}/assets
     mv -v assets/instapy-config.json $HOME > /dev/null 2>&1
     sudo rm -rf $PRIFIX/etc/tor/torrc > /dev/null 2>&1
