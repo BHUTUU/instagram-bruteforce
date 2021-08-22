8a06feb088ca5c37061fa492c64c55efa6f43942
BHUTUU <raj259942@gmail.com>
2021-08-22 05:36:46 +0530

    fix

--- config.sh
+++ config.sh
@@ -44,13 +44,15 @@ case "${OS}" in
     cp -r $PREFIX/lib/python3.9/site-packages/InstagramPy/__p*  ${CWD}/assets/InstagramPy/ > /dev/null 2>&1
     cp -r $PREFIX/lib/python3.9/site-packages//InstagramPy/colors/__pycache__ ${CWD}/assets/InstagramPy/colors/ > /dev/null 2>&1
     rm -rf $PREFIX/lib/python3.9/site-packages/InstagramPy > /dev/null 2>&1
-    mv -v assets/InstagramPy $PREFIX/lib/python3.9/site-packages > /dev/null 2>&1
+    cp -r assets/InstagramPy $PREFIX/lib/python3.9/site-packages > /dev/null 2>&1
     cp -r $PREFIX/lib/python3.9/site-packages/instagram_py-2.0.7-py3.9.egg-info ${CWD}/assets
     mv -v assets/instapy-config.json $HOME > /dev/null 2>&1
     rm -rf $PREFIX/etc/tor/torrc > /dev/null 2>&1
     mv assets/torrc $PREFIX/etc/tor > /dev/null 2>&1
     rm -rf config.sh
+    if [[ -d ~/.instagram-py ]]; then
     mkdir ~/.instagram-py && touch ~/.instagram-py/dump.json > /dev/null 2>&1
+    fi
     ;;
   GNU/Linux)
     printf "${S3} INSTALLING REQUIREMENTS ........${R0}\n"
@@ -68,13 +70,15 @@ case "${OS}" in
     sudo cp -r $PREFIX/lib/python3.9/site-packages/InstagramPy/__p*  ${CWD}/assets/InstagramPy > /dev/null 2>&1
     sudo cp -r $PREFIX/lib/python3.9/site-packages//InstagramPy/colors/__pycache__ ${CWD}/assets/InstagramPy/colors/ > /dev/null 2>&1
     sudo rm -rf $PREFIX/lib/python3.9/site-packages/InstagramPy > /dev/null 2>&1
-    sudo mv -v assets/InstagramPy $PREFIX/lib/python3.9/site-packages > /dev/null 2>&1
+    sudo cp -r assets/InstagramPy $PREFIX/lib/python3.9/site-packages > /dev/null 2>&1
     sudo cp -r $PREFIX/lib/python3.9/site-packages/instagram_py-2.0.7-py3.9.egg-info ${CWD}/assets
     mv -v assets/instapy-config.json $HOME > /dev/null 2>&1
     sudo rm -rf $PRIFIX/etc/tor/torrc > /dev/null 2>&1
     sudo v assets/torrc $PRIFIX/etc/tor > /dev/null 2>&1
     rm -rf config.sh
+    if [[ -d ~/.instagram-py ]]; then
     mkdir ~/.instagram-py && touch ~/.instagram-py/dump.json > /dev/null 2>&1
+    fi
     ;;
   *)
     rm -rf assets config.sh
